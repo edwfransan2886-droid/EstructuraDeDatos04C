34ac7598a3aebeb563fcc2491a957c7e72347f4b
edwfransan2886-droid <edwfransan2886@gmail.com>
2026-03-10 07:05:07 -0500

    Ordenamiento

--- Ejercicios_Ordenamiento/src/Ejercicio_2.java
+++ Ejercicios_Ordenamiento/src/Ejercicio_2.java
@@ -7,39 +7,41 @@ Lógica de Inserción: A medida que el usuario ingresa un número, o una vez lle
 Visualización Paso a Paso: En cada ciclo del ordenamiento, el programa debe imprimir cómo va quedando el arreglo (ej: [10, 25, 5, 30] -> [5, 10, 25, 30]). Esto permite al estudiante ver cómo los elementos se desplazan para abrir espacio al nuevo valor. */
 
 import java.util.Scanner;
+
 public class Ejercicio_2 {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Ingrese la cantidad de libros: ");
         int cantidad = scanner.nextInt();
+
         int[] codigos = new int[cantidad];
 
         for (int i = 0; i < cantidad; i++) {
             System.out.print("Ingrese el codigo del libro " + (i + 1) + ": ");
-            codigos[i] = scanner.nextInt();
+            int nuevo = scanner.nextInt();
+            codigos[i] = nuevo;
+            int key = codigos[i];
+            int j = i - 1;
+
+            while (j >= 0 && codigos[j] > key) {
+                codigos[j + 1] = codigos[j];
+                j--;
+            }
+            codigos[j + 1] = key;
+            System.out.print("Paso " + (i + 1) + ": [ ");
+            for (int num : codigos) {
+                System.out.print(num + " ");
+            }
+            System.out.println("]");
         }
-        insertionSort(codigos);
-        System.out.println("Codigos de libros ordenados:");
+
+        System.out.println("\nCodigos ordenados final:");
         for (int codigo : codigos) {
             System.out.print(codigo + " ");
         }
         scanner.close();
-    }
-    public static void insertionSort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int key = arr[i];
-            int j = i - 1;
-
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = key;
-            System.out.print("Paso " + i + ": ");
-            for (int num : arr) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-        }
     }
 }
